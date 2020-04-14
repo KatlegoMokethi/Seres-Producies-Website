4851805045165ea7b6bd1e2622fc8eadd7862666
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-04-14 23:57:16 +0200

    Add labels page

---
+++ SeresProducoes/Controllers/LabelsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeresProducoes.Controllers
+{
+    public class LabelsController : Controller
+    {
+        public IActionResult Labels()
+        {
+            return View();
+        }
+    }
+}
