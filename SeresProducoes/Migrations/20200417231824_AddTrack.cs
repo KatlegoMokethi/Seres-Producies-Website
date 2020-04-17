0408450934fb3a014ad357fd6ab363d09efa1f4f
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-04-18 01:19:05 +0200

    Run migrations and update the database

---
+++ SeresProducoes/Migrations/20200417231824_AddTrack.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SeresProducoes.Migrations
+{
+    public partial class AddTrack : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Releases",
+                columns: new[] { "Id", "Artist", "Image", "Link", "Title" },
+                values: new object[] { 12, "DEMENTED SOUL", "/images/releases/12_TranscendEP.jpg", "https://www.traxsource.com/title/1321228/transcend-ep", "Transcend EP" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Releases",
+                keyColumn: "Id",
+                keyValue: 12);
+        }
+    }
+}
